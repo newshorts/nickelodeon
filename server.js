0da184268e515855e92b0e43d4a528c394a2b204
Mike Newell <mike@iwearshorts.com>
2012-07-25 17:19:16 -0700

    going home

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+	console.log("loggin connection");
 });
 
 var port = process.env.PORT || 5000;
