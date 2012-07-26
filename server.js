533b4d77a2875cc2797bb237b8d6a135be2e3ba3
Mike Newell <mike@iwearshorts.com>
2012-07-26 11:22:30 -0700

    adding public folder, I have something that works when you visit a URL, next step is to get something that works from slider in a native app

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-	console.log("loggin connection");
-});
-
+var fs = require('fs'),
+    express = require('express'),
+    app = express.createServer(express.logger()),
+    io = require('socket.io').listen(app);
+    
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
+});
+    
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/ipad', function(req, res) {
+    // this is where I forward message from ipad to the rest of the application
+    
+    console.log(req);
+    // io.sockets.emit();
+});
+
+app.post('/ipad', function(req, res) {
+    console.log(req);
+});
+
+io.sockets.on('connection', function(socket) {
+    
+    // i shouldnt need to wait for a socket connection because im only using push updates
+    
 });
