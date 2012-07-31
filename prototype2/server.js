8715fbdcf3992862041a9d06d3d9741acbbc5ce8
Mike Newell <mike@iwearshorts.com>
2012-07-31 04:20:12 -0700

    added videos modified and added prototype 2, it all works

--- prototype2/server.js
+++ prototype2/server.js
@@ -1,6 +1,30 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var fs = require('fs'),
+    express = require('express'),
+    app = express.createServer(express.logger()),
+    io = require('socket.io').listen(app);
+    
+var port = process.env.PORT || 5000;
 
+app.use('/', express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+app.use(app.router);
 
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+app.post('/ipad', function(req, res) {
+    res.send('hello world');
+    io.sockets.emit('output', req.body);
+});
+
+app.post('/splat', function(req, res) {
+    res.send('hello world');
+    io.sockets.emit('splat', req.body);
+});
+
+io.sockets.on('connection', function(socket) {
+    
+    
+    
+});
